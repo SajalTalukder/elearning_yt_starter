388ac1603187f1fa286eaa0ba12a17daaf86840c
Sajal Talukder <sajaltalukder01@gmail.com>
2024-07-17 21:11:03 +0600

    first

---
+++ constant/constant.ts
@@ -0,0 +1,32 @@
+export const navLinks = [
+  {
+    id: 1,
+    url: "#",
+    label: "Home",
+  },
+  {
+    id: 2,
+    url: "#",
+    label: "About",
+  },
+  {
+    id: 3,
+    url: "#",
+    label: "Courses",
+  },
+  {
+    id: 4,
+    url: "#",
+    label: "Testimonial",
+  },
+  {
+    id: 5,
+    url: "#",
+    label: "Blog",
+  },
+  {
+    id: 6,
+    url: "#",
+    label: "Contact",
+  },
+];
